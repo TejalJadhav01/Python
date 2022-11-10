1a2a59ec3c68224939c9758b821923a034a56352
TejalJadhav01 <tejaljadhav2003@gmail.com>
2022-11-10 06:59:06 +0530

    Addition

---
+++ Assignment3/Add.py
@@ -0,0 +1,11 @@
+a = int(input("enter sub1 : "))
+b = int(input("enter sub2 : "))
+c = int(input("enter sub3 : "))
+d = int(input("enter sub4 : "))
+e = int(input("enter sub5 : "))
+
+total = a+b+c+d+e
+per = total/5  #(a+b+c+d+e)/500*100 
+print("total is:", total)
+print("percentage is:" ,per)
+
