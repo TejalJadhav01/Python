4cdb61476cc8ed07ce7b213a4a513e742630a150
TejalJadhav01 <tejaljadhav2003@gmail.com>
2022-11-25 08:18:10 +0530

    Pattern Printing

---
+++ Assignment12/pattern2.py
@@ -0,0 +1,14 @@
+'''
+
+* * * * *
+* * * *
+* * *
+* *
+*
+
+'''
+
+for i in range(5):
+    for j in range(5-i):
+        print("*" , end = " ")
+    print()
---
+++ Assignment12/pattern3.py
@@ -0,0 +1,24 @@
+'''
+
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * *
+* * *
+* *
+*
+
+'''
+
+for i in range(9):
+    if(i<5):
+        for j in range(i+1):
+            print("*", end = " ")
+        print()
+    
+    else:
+        for k in range(9-i):
+            print("*",end = " ")
+        print()
