b6d26ae2c4f9bf2092b247150693183751871c1e
TejalJadhav01 <tejaljadhav2003@gmail.com>
2022-12-01 19:25:30 +0530

    Function

---
+++ Assignment14/Airthmatic_Function.py
@@ -0,0 +1,28 @@
+def add():
+    a = 20
+    b = 30
+    c = a + b
+    print(" Addition is : " , c )
+
+def sub():
+    a = 50
+    b = 30
+    c = a - b
+    print(" Substraction is : " , c )
+
+def div():
+    a = 30
+    b = 30
+    c = a / b
+    print(" Division is : " , c )
+
+def mul():
+    a = 20
+    b = 30
+    c = a * b
+    print(" Multiplication is : " , c )
+
+add()
+sub()
+div()
+mul()
